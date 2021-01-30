25707035ab45bc8c88ded80a9bd2e483f8c26e41
陌上行 <542134326@qq.com>
2021-01-30 22:26:58 +0800

    学习CORS跨域

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 向北
+ * @Date: 2021-01-29 22:12:17
+ * @LastEditors: 向北
+ * @LastEditTime: 2021-01-30 22:25:21
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    let url =
+      "https://mock.mengxuegu.com/mock/6014244a2986555841710109/MiMall/test#!method=get";
+    axios.get(url).then(() => {});
+  },
+};
 </script>
 
 <style>
